c1bb565f2957361905ce5e0907dd518fbea049d4
Siddharth Kumar <siddharth.kumar@einfochips.com>
2022-06-03 15:31:49 +0530

    Updated

--- Hello_yocto.c
+++ Hello_yocto.c
@@ -3,3 +3,15 @@ void main()
 {
   printf("Hello World");
 }
+
+
+S = "${WORKDIR}"
+
+do_compile() {
+        ${CC} hello.c ${LDFLAGS} -o hello
+}
+
+do_install() {
+        install -d ${D}${bindir}
+        install -m 0755 hello ${D}${bindir}
+}
